c8d6998c1cae69d60ef286c3ee9383e851ff3012
mbSmaga <mb.smaga@gmail.com>
2015-05-01 01:01:28 +0200

    algorithm uses less iterations

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -6,6 +6,40 @@ object Application extends App{
   val columns = 7
   val rows = 7
 
+  //holds every possible pair
+  val vector: IndexedSeq[(Int, Int)] = for (i <- 0 until 7; j <- 0 until 7) yield (i, j)
+
+  def vectorOfPairs(configuration: Vector[Piece]) = {
+    def filteredVector(vec: IndexedSeq[(Int, Int)], piece: Piece): IndexedSeq[(Int, Int)] = {
+      val curPos = piece.currentPosition
+      piece.toString match {
+        case "Q" =>
+          vec.filter(pair => (pair._1 != curPos._1)
+            && (pair._2 != curPos._2)
+            && ((pair._2 - curPos._2).abs != (pair._1 - curPos._1).abs))
+        case "B" =>
+          vec.filter(pair => (pair._2 - curPos._2).abs != (pair._1 - curPos._1).abs)
+        case "K" =>
+          vec.filter(pair => ((curPos._1 - pair._1).abs > 1)
+            || ((curPos._2 - pair._2).abs > 1))
+        case "Z" =>
+          vec.filter(pair => ((pair._2 - curPos._2).abs == 2 && (pair._1 - curPos._1).abs == 1)
+            || ((pair._2 - curPos._2).abs == 1 && math.abs(pair._1 - curPos._1).abs == 2))
+        case "R" =>
+          vec.filter(pair => (pair._1 == curPos._1)
+            || (pair._2 == curPos._2))
+        case _ => vec
+      }
+    }
+    def loop(vec: IndexedSeq[(Int, Int)], confi: Vector[Piece]): IndexedSeq[(Int, Int)] = {
+      if (confi.isEmpty) vec
+      else
+        loop(filteredVector(vec, confi.head), confi.tail)
+    }
+    if (configuration.isEmpty) vector
+    else loop(vector, configuration)
+  }
+
   def cyclePieces(pieces: List[Char]): Set[Vector[Piece]] = {
     if (pieces.isEmpty) {
       Set(Vector())
@@ -13,8 +47,7 @@ object Application extends App{
     else {
       for {
         placedPieces <- cyclePieces(pieces.tail)
-        x <- 0 until columns
-        y <- 0 until rows
+        (x, y) <- vectorOfPairs(placedPieces)
         piece = PieceFactory(pieces.head, (x, y))
         if isValidPosition(piece, placedPieces)
       } yield (piece +: placedPieces).sortBy(p => (p.currentPosition._1, p.currentPosition._2))
