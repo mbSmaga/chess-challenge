b44a069e070471e32488f5760ae8fc510712357a
mbSmaga <mb.smaga@gmail.com>
2015-05-03 15:44:57 +0200

    Changed from vector to ListSet, removed redundant contidion

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,3 +1,6 @@
+import scala.annotation.tailrec
+import scala.collection.immutable.ListSet
+
 /**
  * Created by mbsmaga on 28.04.15.
  */
@@ -7,60 +10,64 @@ object Application extends App{
   val rows = 7
 
   //holds every possible pair
-  val vector: IndexedSeq[(Int, Int)] = for (i <- 0 until 7; j <- 0 until 7) yield (i, j)
+  val allLocations: IndexedSeq[(Int, Int)] = for (i <- 0 until 7; j <- 0 until 7) yield (i, j)
 
-  def vectorOfPairs(configuration: Vector[Piece]) = {
+  def validLocations(placedPieces: ListSet[Piece]) = {
     def filteredVector(vec: IndexedSeq[(Int, Int)], piece: Piece): IndexedSeq[(Int, Int)] = {
       val curPos = piece.currentPosition
-      piece.toString match {
-        case "Q" =>
+      piece.name match {
+        case 'Q' =>
           vec.filter(pair => (pair._1 != curPos._1)
             && (pair._2 != curPos._2)
             && ((pair._2 - curPos._2).abs != (pair._1 - curPos._1).abs))
-        case "B" =>
+        case 'B' =>
           vec.filter(pair => (pair._2 - curPos._2).abs != (pair._1 - curPos._1).abs)
-        case "K" =>
+        case 'K' =>
           vec.filter(pair => ((curPos._1 - pair._1).abs > 1)
             || ((curPos._2 - pair._2).abs > 1))
-        case "Z" =>
+        case 'Z' =>
           vec.filter(pair => ((pair._2 - curPos._2).abs == 2 && (pair._1 - curPos._1).abs == 1)
             || ((pair._2 - curPos._2).abs == 1 && math.abs(pair._1 - curPos._1).abs == 2))
-        case "R" =>
+        case 'R' =>
           vec.filter(pair => (pair._1 == curPos._1)
             || (pair._2 == curPos._2))
-        case _ => vec
       }
     }
-    def loop(vec: IndexedSeq[(Int, Int)], confi: Vector[Piece]): IndexedSeq[(Int, Int)] = {
-      if (confi.isEmpty) vec
+    @tailrec
+    def loop(vec: IndexedSeq[(Int, Int)], placedPieces: ListSet[Piece]): IndexedSeq[(Int, Int)] = {
+      if (placedPieces.isEmpty) vec
       else
-        loop(filteredVector(vec, confi.head), confi.tail)
+        loop(filteredVector(vec, placedPieces.head), placedPieces.tail)
     }
-    if (configuration.isEmpty) vector
-    else loop(vector, configuration)
+    if (placedPieces.isEmpty) allLocations
+    else {
+      val validLocations = loop(allLocations, placedPieces)
+      validLocations
+    }
   }
 
-  def cyclePieces(pieces: List[Char]): Set[Vector[Piece]] = {
+  def cyclePieces(pieces: List[Char]): Set[ListSet[Piece]] = {
     if (pieces.isEmpty) {
-      Set(Vector())
+      Set(ListSet())
     }
     else {
       for {
         placedPieces <- cyclePieces(pieces.tail)
-        (x, y) <- vectorOfPairs(placedPieces)
+        // gives only valid pairs
+        (x, y) <- validLocations(placedPieces)
         piece = PieceFactory(pieces.head, (x, y))
         if isValidPosition(piece, placedPieces)
-      } yield (piece +: placedPieces).sortBy(p => (p.currentPosition._1, p.currentPosition._2))
+      } yield (placedPieces + piece)
     }
   }
 
-  def isValidPosition(curPiece: Piece, pieces: Vector[Piece]) = {
+  def isValidPosition(testedPiece: Piece, pieces: ListSet[Piece]) = {
     pieces forall {
-      piece => !(piece.isLocationSafe(curPiece.currentPosition)) && !(curPiece.isLocationSafe(piece.currentPosition))
+      piece => !(testedPiece.isLocationSafe(piece.currentPosition))
     }
   }
 
-  val result = cyclePieces(List('Z', 'K', 'K', 'B', 'B', 'Q', 'Q'))
+  val result = cyclePieces(List('N', 'K', 'K', 'B', 'B', 'Q', 'Q'))
 
   println(result.size)
 }
--- src/main/scala/Piece.scala
+++ src/main/scala/Piece.scala
@@ -1,9 +1,11 @@
 /**
  * Created by mbsmaga on 30.04.15.
  */
-abstract case class Piece(name: Char, currentPosition: (Int, Int)) {
+abstract class Piece {
+  val currentPosition: (Int, Int)
+  val name: Char
 
-  override def toString = name.toString
+//  override def toString = name.toString
 
   def isLocationSafe(testedPosition: (Int, Int)) = {
 
@@ -16,38 +18,38 @@ abstract case class Piece(name: Char, currentPosition: (Int, Int)) {
   def checkLocation (position: (Int, Int)): Boolean
 }
 
-class Queen(currentPosition: (Int, Int)) extends Piece('Q', currentPosition) {
+case class Queen(override val currentPosition: (Int, Int), name: Char) extends Piece {
   override def checkLocation(step: (Int, Int)) =
     step._1 == 0 || step._2 == 0 || step._1 == step._2
 }
 
-class King(currentPosition: (Int, Int)) extends Piece('K', currentPosition) {
+case class King(override val currentPosition: (Int, Int), name: Char) extends Piece {
   override def checkLocation(step: (Int, Int)) =
     step._1 <= 1 && step._2 <= 1
 }
 
-class Bishop(currentPosition: (Int, Int)) extends Piece('B', currentPosition) {
+case class Bishop(override val currentPosition: (Int, Int), name: Char) extends Piece {
   override def checkLocation(step: (Int, Int)) =
     step._1 == step._2
 }
 
-class Rook(currentPosition: (Int, Int)) extends Piece('R', currentPosition) {
+case class Rook(override val currentPosition: (Int, Int), name: Char) extends Piece {
   override def checkLocation(step: (Int, Int)) =
     step._1 == 0 || step._2 == 0
 }
 
-class Zbych(currentPosition: (Int, Int)) extends Piece('N', currentPosition) {
+case class Knight(override val currentPosition: (Int, Int), name: Char) extends Piece {
   override def checkLocation(step: (Int, Int)) =
     (step._1 == 1 && step._2 == 2) || (step._1 == 2 && step._2 == 1)
 }
 object PieceFactory {
   def apply(name: Char, position: (Int, Int)) = {
     name match {
-      case 'Q'   => new Queen(position)
-      case 'K'    => new King(position)
-      case 'B'  => new Bishop(position)
-      case 'R'    => new Rook(position)
-      case 'Z'   => new Zbych(position)
+      case 'Q'   => new Queen(position, name)
+      case 'K'   => new King(position, name)
+      case 'B'   => new Bishop(position, name)
+      case 'R'   => new Rook(position, name)
+      case 'N'   => new Knight(position, name)
     }
   }
 }
