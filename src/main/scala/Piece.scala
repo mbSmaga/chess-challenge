78cf29d4f4cb2def51591dc316710cbe5fa41ca1
mbSmaga <mb.smaga@gmail.com>
2015-05-03 15:45:18 +0200

    Merge branch 'master' of https://github.com/mbSmaga/chess-challenge

--- src/main/scala/Piece.scala
+++ src/main/scala/Piece.scala
@@ -1,11 +1,9 @@
 /**
  * Created by mbsmaga on 30.04.15.
  */
-abstract class Piece {
-  val currentPosition: (Int, Int)
-  val name: Char
+abstract case class Piece(name: Char, currentPosition: (Int, Int)) {
 
-//  override def toString = name.toString
+  override def toString = name.toString
 
   def isLocationSafe(testedPosition: (Int, Int)) = {
 
@@ -18,38 +16,38 @@ abstract class Piece {
   def checkLocation (position: (Int, Int)): Boolean
 }
 
-case class Queen(override val currentPosition: (Int, Int), name: Char) extends Piece {
+class Queen(currentPosition: (Int, Int)) extends Piece('Q', currentPosition) {
   override def checkLocation(step: (Int, Int)) =
     step._1 == 0 || step._2 == 0 || step._1 == step._2
 }
 
-case class King(override val currentPosition: (Int, Int), name: Char) extends Piece {
+class King(currentPosition: (Int, Int)) extends Piece('K', currentPosition) {
   override def checkLocation(step: (Int, Int)) =
     step._1 <= 1 && step._2 <= 1
 }
 
-case class Bishop(override val currentPosition: (Int, Int), name: Char) extends Piece {
+class Bishop(currentPosition: (Int, Int)) extends Piece('B', currentPosition) {
   override def checkLocation(step: (Int, Int)) =
     step._1 == step._2
 }
 
-case class Rook(override val currentPosition: (Int, Int), name: Char) extends Piece {
+class Rook(currentPosition: (Int, Int)) extends Piece('R', currentPosition) {
   override def checkLocation(step: (Int, Int)) =
     step._1 == 0 || step._2 == 0
 }
 
-case class Knight(override val currentPosition: (Int, Int), name: Char) extends Piece {
+class Knight(currentPosition: (Int, Int)) extends Piece('N', currentPosition) {
   override def checkLocation(step: (Int, Int)) =
     (step._1 == 1 && step._2 == 2) || (step._1 == 2 && step._2 == 1)
 }
 object PieceFactory {
   def apply(name: Char, position: (Int, Int)) = {
     name match {
-      case 'Q'   => new Queen(position, name)
-      case 'K'   => new King(position, name)
-      case 'B'   => new Bishop(position, name)
-      case 'R'   => new Rook(position, name)
-      case 'N'   => new Knight(position, name)
+      case 'Q'   => new Queen(position)
+      case 'K'   => new King(position)
+      case 'B'   => new Bishop(position)
+      case 'R'   => new Rook(position)
+      case 'N'   => new Knight(position)
     }
   }
 }
