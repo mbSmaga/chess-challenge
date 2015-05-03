fc3fb4b7c7abd7c66d44ac7b7817168d79639ee3
mbSmaga <mb.smaga@gmail.com>
2015-05-03 16:11:25 +0200

    Changed IndexedSeq to List

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -10,34 +10,34 @@ object Application extends App{
   val rows = 7
 
   //holds every possible pair
-  val allLocations: IndexedSeq[(Int, Int)] = for (i <- 0 until 7; j <- 0 until 7) yield (i, j)
+  val allLocations: List[(Int, Int)] = (for (i <- 0 until 7; j <- 0 until 7) yield (i, j)).toList
 
   def validLocations(placedPieces: ListSet[Piece]) = {
-    def filteredVector(vec: IndexedSeq[(Int, Int)], piece: Piece): IndexedSeq[(Int, Int)] = {
+    def filteredVector(locations: List[(Int, Int)], piece: Piece): List[(Int, Int)] = {
       val curPos = piece.currentPosition
       piece.name match {
         case 'Q' =>
-          vec.filter(pair => (pair._1 != curPos._1)
+          locations.filter(pair => (pair._1 != curPos._1)
             && (pair._2 != curPos._2)
             && ((pair._2 - curPos._2).abs != (pair._1 - curPos._1).abs))
         case 'B' =>
-          vec.filter(pair => (pair._2 - curPos._2).abs != (pair._1 - curPos._1).abs)
+          locations.filter(pair => (pair._2 - curPos._2).abs != (pair._1 - curPos._1).abs)
         case 'K' =>
-          vec.filter(pair => ((curPos._1 - pair._1).abs > 1)
+          locations.filter(pair => ((curPos._1 - pair._1).abs > 1)
             || ((curPos._2 - pair._2).abs > 1))
         case 'Z' =>
-          vec.filter(pair => ((pair._2 - curPos._2).abs == 2 && (pair._1 - curPos._1).abs == 1)
+          locations.filter(pair => ((pair._2 - curPos._2).abs == 2 && (pair._1 - curPos._1).abs == 1)
             || ((pair._2 - curPos._2).abs == 1 && math.abs(pair._1 - curPos._1).abs == 2))
         case 'R' =>
-          vec.filter(pair => (pair._1 == curPos._1)
+          locations.filter(pair => (pair._1 == curPos._1)
             || (pair._2 == curPos._2))
       }
     }
     @tailrec
-    def loop(vec: IndexedSeq[(Int, Int)], placedPieces: ListSet[Piece]): IndexedSeq[(Int, Int)] = {
-      if (placedPieces.isEmpty) vec
+    def loop(locations: List[(Int, Int)], placedPieces: ListSet[Piece]): List[(Int, Int)] = {
+      if (placedPieces.isEmpty) locations
       else
-        loop(filteredVector(vec, placedPieces.head), placedPieces.tail)
+        loop(filteredVector(locations, placedPieces.head), placedPieces.tail)
     }
     if (placedPieces.isEmpty) allLocations
     else {
